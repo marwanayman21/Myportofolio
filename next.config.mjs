9a269eab5091348b154968880be017d43be28e23
marwanayman21 <136925068+marwanayman21@users.noreply.github.com>
2024-11-06 16:54:56 +0200

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+};
+
+export default nextConfig;
+module.exports = {
+    output: 'export',
+    basePath: '/<Myportofolio>',
+    assetPrefix: '/<Myportofolio>/',
+  };
+  
